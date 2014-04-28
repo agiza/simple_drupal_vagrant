f3a00e54c91bb3ae24a05acb6bc03d2a6a4cbc77
Richard <r.chardgoodrow@gmail.com>
2014-04-28 16:33:55 -0500

    Switching to wget to grab drupal 7.27

--- service-setup.sh
+++ service-setup.sh
@@ -10,4 +10,15 @@ sudo mysql_secure_installation
 sudo service mysql restart
 sudo service apache2 restart
 
+# drush 
+sudo apt-get install drush -y
+echo ::::: You may want to run sudo drush self-update
 
+
+# permissions
+sudo chmod 2775 -R /var/www
+sudo chown www-data:www-data -R /var/www
+
+# user mod
+sudo usermod -a -G www-data vagrant
+echo ::::: Your user was added to the www-data group but you may have to log out and back in for changes to take place. 
